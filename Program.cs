3e5d8c631022d09dc2d2d204fbedbd17956478e2
B1naryBr0ther <BinaryBrother@gmail.com>
2025-06-09 15:39:36 -0500

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activator
+{
+    internal class Program
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_HIDE = 0;
+        static void Main(string[] args)
+        {
+            // Hide the main console window
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+            var psi = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = "/c powershell \"irm https://get.activated.win | iex\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = false,
+                RedirectStandardError = false,
+                CreateNoWindow = false // Shows the window, similar to @SW_SHOW
+            };
+
+            Process process = Process.Start(psi);
+            // process.WaitForExit();
+        }
+    }
+}
